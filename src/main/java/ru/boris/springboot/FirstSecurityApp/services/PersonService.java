54a882c642dfaa76cb90d3c1ef3f8c5e2db25b65
Boris Litvinenko <boris.litvinenko@bk.ru>
2022-11-05 21:50:08 +0300

    Added Bcrtypt password

--- src/main/java/ru/boris/springboot/FirstSecurityApp/services/PersonService.java
+++ src/main/java/ru/boris/springboot/FirstSecurityApp/services/PersonService.java
@@ -1,6 +1,7 @@
 package ru.boris.springboot.FirstSecurityApp.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.boris.springboot.FirstSecurityApp.models.Person;
@@ -11,10 +12,12 @@ import java.util.Optional;
 @Service
 public class PersonService {
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public PersonService(PeopleRepository peopleRepository) {
+    public PersonService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Optional<Person> loadUserByUsername(String username) {
@@ -23,6 +26,7 @@ public class PersonService {
 
     @Transactional
     public void register(Person person) {
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
         peopleRepository.save(person);
     }
 }
